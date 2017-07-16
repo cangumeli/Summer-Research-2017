d70128db010f61f12a6690a9cecae109e2f9742d
cangumeli <cgumeli@ku.edu.tr>
2017-07-16 05:15:37 +0300

    GRU, dropout and multi-arg mc

--- knet.modules/knet.modules.jl
+++ knet.modules/knet.modules.jl
@@ -115,7 +115,7 @@ end
 #Macro for calling types directly
 macro mc(expr)
    if typeof(expr) == Expr && expr.head == :call
-      return Expr(:call, :forward, :context, expr.args[1], expr.args[2])
+      return Expr(:call, :forward, :context, expr.args[1], expr.args[2:end]...)
    end
    return expr
 end
@@ -327,7 +327,31 @@ type BatchNorm4 <: AbstractBatchNorm
 end
 
 
+type Dropout <: Module
+   keep_prob::AbstractFloat
+   mode::Symbol
+   Dropout(keep_prob::AbstractFloat; mode=:train) = new(keep_prob, mode)
+end
 
+function forward(context, m::Dropout, x)
+   assert(m.mode in [:train, :test])
+   tx = typeof(AutoGrad.getval(x))
+   sizex = size(AutoGrad.getval(x))
+   etx = eltype(AutoGrad.getval(x))
+   p = etx(m.keep_prob)
+   if p == 0
+      tx(zeros(sizex))
+   elseif p == 1
+      x
+   elseif m.mode === :train
+      flt = tx(rand(sizex) .<= p)
+      x .* flt
+   else # test mode
+      p .* x
+   end
+end
+
+#---- Containers ----
 abstract Container <: Module
 
 function layers(c::Container)
@@ -413,8 +437,62 @@ function reset_state!(rnn::AbstractRNN)
    rnn.history = []
 end
 
-function hidden_state(rnn::AbstractRNN)
-   AutoGrad.getval(rnn.history[end])
+function hidden_state(rnn::AbstractRNN; unbox=false)
+   unbox ? AutoGrad.getval(rnn.history[end]) : rnn.history[end]
+end
+
+# The GRU layer
+type GRU <: AbstractRNN
+   history::Array{Any, 1}
+   h2z::Linear
+   i2z::Linear
+   h2r::Linear
+   i2r::Linear
+   h2n::Linear
+   i2n::Linear
+   input_size::Int
+   hidden_size::Int
+   function GRU(input_size::Int, hidden_size::Int; bias=true)
+      h2z = Linear(hidden_size, hidden_size; bias=bias)
+      i2z = Linear(input_size, hidden_size; bias=bias)
+      h2r = Linear(hidden_size, hidden_size; bias=bias)
+      i2r = Linear(input_size, hidden_size; bias=bias)
+      h2n = Linear(hidden_size, hidden_size; bias=bias)
+      i2n = Linear(input_size, hidden_size; bias=bias)
+      new([], h2z, i2z, h2r, i2r, h2n, i2n, input_size, hidden_size)
+   end
+end
+
+# Single time step of a GRU
+# TODO: checkout masking
+function forward(context, m::GRU, x, mask=nothing)
+   if length(m.history) == 0
+      tx = typeof(AutoGrad.getval(x))
+      h0 = zeros(m.hidden_size, size(x)[end])
+      push!(m.history, tx(h0))
+   end
+   ht_1 = hidden_state(m)
+   if mask != nothing
+      if ndims(mask) == 1
+         mask = reshape(mask, (1, size(mask)[1]))
+      end
+      assert(size(mask) == (1, size(x)[2]))
+      # Remove error when sure about masking
+      # error("Masking is not supported yet")
+      #= The idea is remove the effect of padded values
+      from the output and freeze their state
+      but I'm not sure. =#
+      z = sigm(@mc(m.i2z(x)) .+ @mc(m.h2z(ht_1))) .* mask
+      r = sigm(@mc(m.i2r(x)) .+ @mc(m.h2r(ht_1))) .* mask
+      n = tanh(@mc(m.i2n(x)) .+ r .* @mc(m.h2n(ht_1))) .* mask
+      ht = (1 .- z) .* n .+ z .* ht_1 .+ (1 .- mask) .* ht_1
+   else
+      z = sigm(@mc(m.i2z(x)) .+ @mc(m.h2z(ht_1)))
+      r = sigm(@mc(m.i2r(x)) .+ @mc(m.h2r(ht_1)))
+      n = tanh(@mc(m.i2n(x)) .+ r .* @mc(m.h2n(ht_1)))
+      ht = (1 .- z) .* n .+ z .* ht_1
+   end
+   push!(m.history, ht)[end]
 end
 
 # To be continued
