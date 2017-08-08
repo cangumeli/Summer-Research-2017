55cbd6f8a1fa4142094a6735b0741c3f3fcef0af
Can Gümeli <can.gumeli@loopcake.com>
2017-08-08 18:54:00 +0300

    Timing

--- multiGPU/uva_test.cu
+++ multiGPU/uva_test.cu
@@ -2,66 +2,19 @@
 #include <stdlib.h> 
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <math.h>
 
-/*__global__ void vectorAdd(float *dst, float *v1, float *v2)
-{
-  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  dst[idx] = v1[idx] + v2[idx];
-}
-
-
-int execute()
-{
-  float *v0, *v1, *dst;
-  float *dst_host, *v0_host, *v1_host;
-  size_t size = sizeof(float)*64*64;
-  //cudaSetDevice(0);
-  dst_host = (float *)malloc(size);
-  cudaMalloc(&v0, size);
-  cudaMemset(v0, 1.0, size);
-  
-  //cudaSetDevice(1);
-  cudaMalloc(&dst, size);
-  cudaMalloc(&v1, size);
-  cudaMemset(v1, 2.0, size);
-  cudaDeviceSynchronize();
-  //Enable the peer access from 1 to 0
-  //cudaDeviceEnablePeerAccess(1, 0);
-  //Launch kernel in 1
-  vectorAdd<<<64, 64>>>(dst, v0, v1);
-  //Take result to the cpu
-  cudaMemcpy(dst_host, dst, size, cudaMemcpyDeviceToHost);
-  cudaDeviceSynchronize();
-  float sum = 0.0;
-  for (int i = 0; i < 64*64; ++i)
-    sum += dst_host[i];
-  if (sum == 64*64*3) {
-    printf("Passed...\n");
-  } else {
-    printf("Sum: %f\n", sum);
-    printf("Failed...\n");
-  }
-  // Clean up
-  cudaFree(v0);
-  cudaFree(v1);
-  cudaFree(dst);
-  free(dst_host);
-  return 0;
-  }*/
-
-  __global__ void simpleKernel(float *dst, float *src1, float *src2)
+__global__ void simpleKernel(float *dst, float *src1, float *src2)
 {
   const int idx = blockIdx.x * blockDim.x + threadIdx.x;
   //float temp = src[idx];
   dst[idx] = src1[idx] + src2[idx];
 }
 
-int execute()
+int execute_uva(bool copy=false, bool print=false)
 {
   float *src1,*src2, *dst;
-  float *dsrc1, *dsrc2, *ddst;
-  size_t rsize = 8;
+  float *dsrc1, *dsrc2, *ddst, *dsrc2_1;
+  size_t rsize = 256;
   size_t size = sizeof(float) * rsize * rsize;
   //cpu buffers
   src1 = (float *)malloc(size);
@@ -85,29 +38,54 @@ int execute()
   
   //Launch the kernel
   cudaSetDevice(0);
-  simpleKernel<<<rsize, rsize>>>(ddst, dsrc1, dsrc2);
-  cudaMemcpy(dst, ddst, size, cudaMemcpyDeviceToHost);
-  for(int i = 0; i < rsize*rsize; ++i) {
-    printf("%d: %f\n", i, dst[i]);
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start);
+  if (copy) { 
+    //Add all the overhead of copying to the times
+    //including memory allocation
+    cudaMalloc(&dsrc2_1, size);
+    cudaMemcpy(dsrc2_1, dsrc2, size, cudaMemcpyDefault);
+    simpleKernel<<<rsize, rsize>>>(ddst, dsrc2_1, dsrc2);
+  } else {
+    simpleKernel<<<rsize, rsize>>>(ddst, dsrc1, dsrc2);
   }
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+  float time_ms = 0;
+  cudaEventElapsedTime(&time_ms, start, stop);
+  cudaMemcpy(dst, ddst, size, cudaMemcpyDeviceToHost);
+  if (print)
+    for(int i = 0; i < rsize*rsize; ++i) {
+      printf("%d: %f\n", i, dst[i]);
+    }
+  printf("Last item: %f\n", dst[rsize*rsize-1]);
+  printf("Elapsed time: %f\n", time_ms); 
+  // clean gpu buffers
   cudaFree(ddst);
   cudaFree(dsrc1);
+  if (copy) cudaFree(dsrc2_1);
+  // Just in case
+  cudaSetDevice(1);
   cudaFree(dsrc2);
+  // clean cpu buffers
   free(src1);
   free(src2);
   free(dst);
   return 0;
 }
 
+
 int main()
 {
-  int canAccess;
-  cudaDeviceCanAccessPeer(&canAccess, 1, 0);
-  printf("Access status: %d\n", canAccess);
-  cudaDeviceCanAccessPeer(&canAccess, 0, 1);
-  printf("Access status: %d\n", canAccess);
-  if (canAccess) {
-    execute();
+  int canAccess10, canAccess01;
+  cudaDeviceCanAccessPeer(&canAccess10, 1, 0);
+  printf("Access status: %d\n", canAccess10);
+  cudaDeviceCanAccessPeer(&canAccess01, 0, 1);
+  printf("Access status: %d\n", canAccess01);
+  if (canAccess10 && canAccess01) {
+    execute_uva(true);
   }
   return 0;
 }
